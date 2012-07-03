986a2e9e578d8eeede66c2556c9d7bb65d2fe816
Frederik Claus <f.v.claus@googlemail.com>
2012-07-03 12:08:45 +0200

    configured logging

--- photomap/settings.py
+++ photomap/settings.py
@@ -17,6 +17,56 @@ PHOTO_PATH = os.path.join(STATIC_PATH, "photo")
 LOG_PATH = os.path.join(PROJECT_PATH, "main.log")
 MANAGERS = ADMINS
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.db.backends':{
+            'handlers': ['console'],
+            'level' : 'DEBUG',
+            'propagate': False,
+        },
+        'map': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+    }
+}
+
 import sys
 if 'test' in sys.argv:
     DATABASES = {
@@ -150,28 +200,5 @@ INSTALLED_APPS = (
 )
 
 
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
 
 AUTH_PROFILE_MODULE = "map.model.userprofile.UserProfile"
