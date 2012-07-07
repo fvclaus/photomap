22cdbaeaaaa5ca1dbdc7f78d671760f6ef2b8042
Frederik Claus <f.v.claus@googlemail.com>
2012-07-07 15:24:28 +0200

    added argument to success()

--- photomap/map/controller/message.py
+++ photomap/map/controller/message.py
@@ -7,8 +7,13 @@ Created on Jun 30, 2012
 from django.http import HttpResponse, HttpResponseBadRequest
 import json
 
-def success():
-    return HttpResponse(jsonify({"success" :  True}), content_type = "text/json")
+def success(**kwargs):
+    data = {
+            "success" : True}
+    for k in kwargs.keys():
+        data[k] = kwargs[k]
+        
+    return HttpResponse(jsonify(data), content_type = "text/json")
 
 def error(msg):
     return HttpResponse(jsonify({"success": False, "error" : msg}), content_type = "text/json")
