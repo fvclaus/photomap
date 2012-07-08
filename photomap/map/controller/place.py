b75329aa0ce96ed986554e8a4be7f0e82fd8340a
Frederik Claus <f.v.claus@googlemail.com>
2012-07-08 12:59:57 +0200

    finished #24

---
+++ photomap/map/controller/place.py
@@ -0,0 +1,20 @@
+'''
+Created on Jul 8, 2012
+
+@author: fredo
+'''
+
+
+from message import success, error
+
+
+def insert(request):
+    pass
+
+def update(request):
+    pass
+
+
+def delete(request):
+    pass
+
---
+++ photomap/map/test/place.py
@@ -0,0 +1,87 @@
+'''
+Created on Jul 8, 2012
+
+@author: fredo
+'''
+
+from simpletestcase import SimpleTestCase
+from django.test.client import Client
+from decimal import Decimal
+import json
+from map.model.place import Place
+from map.model.photo import Photo
+import os
+
+class PlaceControllerTest(SimpleTestCase):
+    
+    model = Place
+    
+    def test_insert(self):
+        #=======================================================================
+        # define url for post requests
+        #=======================================================================
+        self.url = "/insert-place"
+        #=======================================================================
+        # without description
+        #=======================================================================
+        data = {"lat": Decimal(-48.01230012),
+                "lon": Decimal(8.0123123),
+                "title": "Next to EO", }
+        (place, content) = self.assertCreates(data)
+        self.assertEqual(place.title, content["title"])
+        #=======================================================================
+        # with description 
+        #=======================================================================
+        data["description"] = "Some description, blah, blah..."
+        (place, content) = self.assertCreates(data)
+        self.assertEqual(place.title, content["description"])
+        #=======================================================================
+        # something invalid
+        #=======================================================================
+        del data["lat"]
+        self.assertError(data)
+        #=======================================================================
+        # something else invalid
+        #=======================================================================
+        del data["lon"]
+        self.assertError(data)
+    
+        
+    def test_update(self):
+        self.url = "/update-photo"
+        #=======================================================================
+        # without description
+        #=======================================================================
+        data = {"id": 1,
+                "title": "Some other title"}
+        (place, content) = self.assertUpdates(data)
+        self.assertEqual(place.title, data["title"])
+        #=======================================================================
+        # with description
+        #=======================================================================
+        data["description"] = "Some other description"
+        (place, content) = self.assertUpdates(data)
+        self.assertEqual(place.description, data["description"])
+        #=======================================================================
+        # something invalid
+        #=======================================================================
+        data["id"] = 9999
+        self.assertError(data)
+        
+    
+    def test_delete(self):
+        self.url = "/delete-place"
+        #=======================================================================
+        # valid request
+        #=======================================================================
+        place = Place.objects.get(pk = 1)
+        photos = Photo.objects.get(place = place)
+        self.assertDeletes({"id": place.pk})
+#        assert on delete cascade
+        for photo in photos:
+            self.assertRaises(Photo.DoesNotExist, Photo.objects.get, {"pk": photo.pk})
+            self.assertFalse(os.path.exists(photo.photo.path))
+        #=======================================================================
+        # not valid
+        #=======================================================================
+        self.assertError({"id": 9999})
