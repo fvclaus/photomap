f685948257541982a67dc182ab05b099875f93a3
Marc Roemer <marcleonroemer@gmail.com>
2012-07-18 19:07:49 +0200

    adjusted pm-header: divided into 3 boxes (logo, title, controls); resized and repositioned boxes

--- photomap/static/javascripts/main/UI/UIPanel.js
+++ photomap/static/javascripts/main/UI/UIPanel.js
@@ -5,18 +5,22 @@ UIPanel = function (){
     this.$topPanel = $('.mp-top-panel');
     this.$bottomPanel = $('.mp-bottom-panel');
     this.$footer = $("#mp-footer");
+    this.$header = $("#mp-header");
+    this.$title = this.$header.find(".mp-page-title");
 };
 
 UIPanel.prototype = {
     
     init : function(){
 	this.repositionBottomPanel();
-	this.resizeBottomPanelLinks(this.$bottomPanel)
+	this.resizeBottomPanelLinks(this.$bottomPanel);
+	this.resizePageTitle(this.$title);
     },
 	
     getControlsBarHeight : function(){
 	return this.$topPanel.height();
     },
+    
     
     resizeBottomPanelLinks : function(bottomPanel){
 	    text = bottomPanel.text();
@@ -24,6 +28,14 @@ UIPanel.prototype = {
 	    height = bottomPanel.height();
 	    size = this.tools.calculateFontSize(text,width,height);
 	    bottomPanel.css("fontSize",size + "px");
+    },
+    
+    resizePageTitle : function(titlebox){
+	text = titlebox.text();
+	width = titlebox.width() * 0.7;
+	height = titlebox.height() * 0.7;
+	size = this.tools.calculateFontSize(text,width,height);
+	titlebox.css("fontSize",size + "px");
     },
     
     repositionBottomPanel : function(){
