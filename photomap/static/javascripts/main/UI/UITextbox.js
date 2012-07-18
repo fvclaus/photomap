3662beef42850effe9a431d27aa12773394e9963
Marc Roemer <marcleonroemer@gmail.com>
2012-07-18 11:50:49 +0200

    added new class for pages with map as background (UITextbox)

---
+++ photomap/static/javascripts/main/UI/UITextbox.js
@@ -0,0 +1,21 @@
+UITextbox = function(){
+  
+  this.$container = $("div.mp-content-container");
+  this.$login = this.$container.find("#mp-login");
+  this.$register = this.$container.find("#mp-register");
+};
+
+UITextbox.prototype = {
+  
+  init : function(){
+    this.repositionLoginRegistrationForms();
+  },
+  
+  repositionLoginRegistrationForms : function(){
+    height = this.$container.height();
+    loginMarginTop = height - this.$login.height() * 0.5;
+    registerMarginTop = height - this.$register.height() * 0.5;
+    this.$login.css('margin-top',loginMarginTop);
+    this.$register.css('margin-top',registerMarginTop);
+  },
+};
