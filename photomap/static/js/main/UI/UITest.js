e37f1a835d3b1c64a7e0a5fabcbb215498a7769a
fvclaus <f.v.claus@googlemail.com>
2012-07-26 12:37:03 +0200

    test, test, javascript test

--- photomap/static/js/main/UI/UITest.js
+++ photomap/static/js/main/UI/UITest.js
@@ -1,30 +1,43 @@
 $(document).ready(function(){
 
     $("button.mp-show-place").click(function(){
-	place = getPlace();
+	place = selectPlace();
 	place.triggerClick();
     });
+    $("button.mp-show-photo").click(function(){
+	photo = selectPhoto();
+	photo.triggerClick();
+    });
     $("button.mp-update-place").click(function(){
-	place = getPlace();
-	main.getUI().getState().setCurrentPlace(place);
+	selectPlace();
+	main.getUI().getControls().$update.trigger("click");
     });
     $("button.mp-update-photo").click(function(){
-	photo = getPhoto();
-	main.getUI().getState().setCurrentPhoto(photo);
+	selectPhoto();
+	main.getUI().getControls().$update.trigger("click");
     });
-    
+    $("button.mp-delete-place").click(function(){
+	selectPlace();
+	main.getUI().getControls.$delete.trigger("click");
+    });
+    $("button.mp-delete-photo").click(function(){
+	selectPhoto();
+	main.getUI().getControls().$delete.trigger("click");
+    });
 });
 
 /*
   returns the first place
 */
-function getPlace(){
+function selectPlace(){
     places = main.getMap().places;
     if(places.length == 0){
 	alert ("Need places for test!");
 	return null;
     }
     else{
+	main.getUI().getState().setCurrentPlace(places[0]);
+	main.getUI().getControls().setModifyPlace(true);
 	return places[0];
     }
 }
@@ -32,9 +45,11 @@ function getPlace(){
 /*
   returns the last photo of getPlace()
 */
-function getPhoto(){
-    photos = getPlace().photos;
+function selectPhoto(){
+    photos = selectPlace().photos;
     photo = photos[photos.length -1];
+    main.getUI().getState().setCurrentPhoto(photo);
+    main.getUI().getControls().setModifyPhoto(true);
     return photo;
 }
 	
