c47b3919058ab7cc83eca6653359b3e3b239550c
Frederik Claus <f.v.claus@googlemail.com>
2012-09-28 15:11:16 +0200

    added middleware

---
+++ photomap/pm/middleware.py
@@ -0,0 +1,32 @@
+'''
+Created on Sep 28, 2012
+
+@author: fredo
+'''
+
+import logging
+import re
+from django.shortcuts import render_to_response
+
+class NoSupportMiddleware():
+    IS_FF = re.compile("Firefox/(?P<version>\d{2})")
+    MIN_VERSION = 10
+    
+    def process_request(self, request):
+        logger = logging.getLogger(__name__)
+        user_agent = request.META["HTTP_USER_AGENT"]
+        logger.debug("User agent is %s" % user_agent)
+        match = self.IS_FF.search(user_agent)
+    
+        if not match:
+            logger.debug("Browser is not firefox. Redirecting...")
+            return render_to_response("not-supported.html")
+        else:
+            version = match.group("version")
+            
+            if  version < self.MIN_VERSION:
+                logger.debug("FF in version %d. Must be at least %d" % (version, self.MIN_VERSION))
+                return render_to_response("not-supported.html")
+            
+            return None
+        
