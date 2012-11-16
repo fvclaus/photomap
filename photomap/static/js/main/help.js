52a126393505de8f4eb9f6377e8bca2c32bc60e6
Frederik Claus <f.v.claus@googlemail.com>
2012-11-16 21:31:19 +0100

    jslint syntax for help.js

--- photomap/static/js/main/help.js
+++ photomap/static/js/main/help.js
@@ -1,40 +1,47 @@
-function toggleFAQAnswers($question){
-   $answer = $question.next('p');
-   $answers = $(".mp-faq-answer");
+/*jslint  */
+/*global $ */
+
+"use strict";
+
+var $answer, $answers, $desc, $descriptions, trigger;
+
+function toggleFAQAnswers($question) {
+    $answer = $question.next('p');
+    $answers = $(".mp-faq-answer");
 
    // hide all other answers
-   $answers.not($answer).slideUp(150);
-   if ($answer.is(":visible")){
-      $answer.slideUp(150);
-   }
-   else{
-      $answer.slideDown(300);
-   }
-};
-function toggleTutorialDesc($topic){
-   $desc = $topic.next('p');
-   $descriptions = $(".mp-tutorial-subtopic-desc");
+    $answers.not($answer).slideUp(150);
+    if ($answer.is(":visible")) {
+        $answer.slideUp(150);
+    } else {
+        $answer.slideDown(300);
+    }
+}
 
-   // hide all other descriptions
-   $descriptions.not($desc).slideUp(150);
-   if ($desc.is(":visible")){
-      $desc.slideUp(150);
-   }
-   else{
-      $desc.slideDown(300);
-   }
-};
+function toggleTutorialDesc($topic) {
+    $desc = $topic.next('p');
+    $descriptions = $(".mp-tutorial-subtopic-desc");
 
-function initialize(){
+    // hide all other descriptions
+    $descriptions.not($desc).slideUp(150);
+    if ($desc.is(":visible")) {
+        $desc.slideUp(150);
+    } else {
+        $desc.slideDown(300);
+    }
+}
 
-   $(".mp-faq-question").bind('click', function(){
-      trigger = $(this);
-      toggleFAQAnswers(trigger);
-   });
-   $(".mp-tutorial-subtopic").bind('click', function(){
-      trigger = $(this);
-      toggleTutorialDesc(trigger);
-   });
+function initialize() {
 
-};
+    $(".mp-faq-question").bind('click', function () {
+        trigger = $(this);
+        toggleFAQAnswers(trigger);
+    });
+    $(".mp-tutorial-subtopic").bind('click', function () {
+        trigger = $(this);
+        toggleTutorialDesc(trigger);
+    });
+
+}
+
 
