bca3ea8105a7c851ba30fd46a55697f2e13484d9
Frederik Claus <f.v.claus@googlemail.com>
2012-11-17 11:26:58 +0100

    main/help.js without jslint errors

--- photomap/static/js/main/help.js
+++ photomap/static/js/main/help.js
@@ -6,41 +6,41 @@
 var $answer, $answers, $desc, $descriptions, trigger;
 
 function toggleFAQAnswers($question) {
-    $answer = $question.next('p');
-    $answers = $(".mp-faq-answer");
+   $answer = $question.next('p');
+   $answers = $(".mp-faq-answer");
 
    // hide all other answers
-    $answers.not($answer).slideUp(150);
-    if ($answer.is(":visible")) {
-        $answer.slideUp(150);
-    } else {
-        $answer.slideDown(300);
-    }
+   $answers.not($answer).slideUp(150);
+   if ($answer.is(":visible")) {
+      $answer.slideUp(150);
+   } else {
+      $answer.slideDown(300);
+   }
 }
 
 function toggleTutorialDesc($topic) {
-    $desc = $topic.next('p');
-    $descriptions = $(".mp-tutorial-subtopic-desc");
+   $desc = $topic.next('p');
+   $descriptions = $(".mp-tutorial-subtopic-desc");
 
     // hide all other descriptions
-    $descriptions.not($desc).slideUp(150);
-    if ($desc.is(":visible")) {
-        $desc.slideUp(150);
-    } else {
-        $desc.slideDown(300);
-    }
+   $descriptions.not($desc).slideUp(150);
+   if ($desc.is(":visible")) {
+      $desc.slideUp(150);
+   } else {
+      $desc.slideDown(300);
+   }
 }
 
 function initialize() {
 
-    $(".mp-faq-question").bind('click', function () {
-        trigger = $(this);
-        toggleFAQAnswers(trigger);
-    });
-    $(".mp-tutorial-subtopic").bind('click', function () {
-        trigger = $(this);
-        toggleTutorialDesc(trigger);
-    });
+   $(".mp-faq-question").bind('click', function () {
+      trigger = $(this);
+      toggleFAQAnswers(trigger);
+   });
+   $(".mp-tutorial-subtopic").bind('click', function () {
+      trigger = $(this);
+      toggleTutorialDesc(trigger);
+   });
 
 }
 
