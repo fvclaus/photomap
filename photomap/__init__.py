8c105f24e0e4d965dae34f1989152c2f295fcda7
Frederik Claus <f.v.claus@googlemail.com>
2013-01-09 00:42:44 +0100

    removed eclipse-django setup hacks

--- photomap/__init__.py
+++ photomap/__init__.py
@@ -2,6 +2,6 @@ import sys
 import settings
 from django.core.management import setup_environ
 
-setup_environ(settings)
-
-sys.path.append(settings.PROJECT_PATH)
+#setup_environ(settings)
+#
+#sys.path.append(settings.PROJECT_PATH)
