8a99ee3e97e7a70aa6a1c84b869e3d5616bc7f42
Frederik Claus <f.v.claus@googlemail.com>
2013-01-11 15:49:23 +0100

    updated logo, added clear.sql

---
+++ photomap/clear.sql
@@ -0,0 +1,9 @@
+BEGIN;
+DROP TABLE "pm_userprofile";
+DROP TABLE "pm_share";
+DROP TABLE "pm_photo";
+DROP TABLE "pm_place";
+DROP TABLE "pm_invitation";
+DROP TABLE "pm_feed";
+DROP TABLE "pm_album";
+COMMIT;
