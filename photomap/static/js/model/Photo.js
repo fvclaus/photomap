55a975a1f8630fbb81662aee235fb05e5030e503
Frederik Claus <f.v.claus@googlemail.com>
2013-01-30 14:09:29 +0100

    removed the  check in openPhoto again

--- photomap/static/js/model/Photo.js
+++ photomap/static/js/model/Photo.js
@@ -43,9 +43,10 @@ Photo.prototype = {
    openPhoto : function () {
       var $thumb = main.getUI().getGallery().getImageBySource(this.source);
       main.getUIState().setCurrentLoadedPhoto(this);
-      if ($thumb.length === 0){
-         throw new Error("Photo must always have a $thumb");
-      }
+      //TODO uncomment this when getImageBySource works again
+      // if ($thumb.length === 0){
+      //    throw new Error("Photo must always have a $thumb");
+      // }
       //TODO this does not seem to work anymore with the new .on jQuery listener style
       $thumb.trigger("click");
    }
