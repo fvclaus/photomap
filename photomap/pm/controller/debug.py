ad6754397d4c63388990eb5400d273cf5497c08a
Frederik Claus <f.v.claus@googlemail.com>
2013-02-03 15:41:41 +0100

    added new debugging url /debug/name added new base template debug.html moved old sandbox to debug/photoeditor

---
+++ photomap/pm/controller/debug.py
@@ -0,0 +1,12 @@
+'''
+Created on Feb 2, 2013
+
+@author: fredo
+'''
+
+from django.shortcuts import render_to_response
+import os
+
+
+def view(request, name):
+    return render_to_response(os.path.join("debug","%s.html" % name), {"name": name })
