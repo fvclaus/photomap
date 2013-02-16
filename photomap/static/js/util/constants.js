043389ebf847cf5b04c5d2cfdcb4cb5a850f9249
Frederik Claus <f.v.claus@googlemail.com>
2013-02-16 12:54:50 +0100

    added default camera icon again ^^

--- photomap/static/js/util/constants.js
+++ photomap/static/js/util/constants.js
@@ -12,25 +12,19 @@ PAGE_MAPPING = {
    "/view-album" : ALBUM_VIEW
 };
 
-MARKER_DEFAULT_ICON = "static/images/camera-roadmap.png";
+
 
 ALBUM_VISITED_ICON = "static/images/camera-visited.png";
 
 PLACE_VISITED_ICON = "static/images/camera-visited.png";
 PLACE_SELECTED_ICON = "static/images/camera-current.png";
-PLACE_UNSELECTED_ICON = "static/images/camera-roadmap.png";
+PLACE_UNSELECTED_ICON = "static/images/camera-not-visited.png";
 PLACE_DISABLED_ICON = "static/images/camera-disabled.png";
+
+MARKER_DEFAULT_ICON = PLACE_UNSELECTED_ICON;
 
 TEMP_TITLE_KEY = "temp_title";
 TEMP_DESCRIPTION_KEY = "temp_description";
 
 ZOOM_LEVEL_CENTERED = 13;
 
-ALLOWED_UPLOAD_FILE_TYPES = ['image/png', 'image/jpeg'];
-
-ERRORS = {
-   'TOO_MANY_PHOTOS': 'You may just upload one Photo at a time!',
-   'UNALLOWED_FILE_TYPE': 'File-Type not allowed. Just *.jpeg and *.png are supported.',
-   'NO_FILE_API_SUPPORT': 'The File APIs (DragnDrop) are not fully supported in this browser. Please upgrade to a newer version.',
-   'NO_XHR2_SUPPORT': "Error initializing XMLHttpRequest! Not compatible with this browser. Please upgrade to a newer version."
-};
