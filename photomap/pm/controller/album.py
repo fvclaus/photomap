f0575f9bcfc20b3e20775a5f73bb0ca458a78589
Frederik Claus <f.v.claus@googlemail.com>
2013-03-03 17:24:10 +0100

    fixed non-extisting template errors

--- photomap/pm/controller/album.py
+++ photomap/pm/controller/album.py
@@ -93,7 +93,7 @@ def view(request, secret, album_id):
             #TODO better name
             logger.debug("Secret does not match.")
             logger.debug("%s is not %s" % (secret, album.secret))
-            return render_to_response("album-view-failure.html")
+            return render_to_response("album-share-failure.html")
     
         if request.method == "GET":
             # user owns the album
@@ -105,9 +105,9 @@ def view(request, secret, album_id):
             # album does not has a password yet
             if not hashers.is_password_usable(album.password):
                 logger.debug("Album does not has a password yet.")
-                return render_to_response("album-view-failure.html")
+                return render_to_response("album-share-failure.html")
             
-            return render_to_response("album-view-login.html")
+            return render_to_response("album-share-login.html")
         else:
             password = request.POST["password"]
             
@@ -116,10 +116,10 @@ def view(request, secret, album_id):
                 return redirect("/album/view/%s-%d" % (album.secret, album_id))
             else:
                 logger.debug("Password %s is incorrect." % password)
-                return render_to_response("album-view-login.html", {"password_incorrect_error": "Passwort is not correct."})
+                return render_to_response("album-share-login.html", {"password_incorrect_error": "Passwort is not correct."})
     except Exception, e:
         logger.info(str(e))
-        return render_to_response("album-view-failure.html")
+        return render_to_response("album-share-failure.html")
 
 
 
