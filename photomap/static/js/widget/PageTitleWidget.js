50c5ec86dfef12f4534c0db97e758b3ee0106159
Frederik Claus <f.v.claus@googlemail.com>
2013-07-17 14:10:40 +0200

    js: Removed UIState/ClientState dependency from many classes

---
+++ photomap/static/js/widget/PageTitleWidget.js
@@ -0,0 +1,26 @@
+/*global define, $*/
+
+"use strict";
+
+define(["dojo/_base/declare",
+        "util/Communicator",
+        "dojo/domReady!"],
+       function (declare, communicator) {
+          return declare(null, {
+             constructor : function () {
+                this.$pageTitle = $("#mp-page-title");
+                this._bindListener();
+             },
+             _bindListener : function () {
+                var instance = this;
+                this.$pageTitle.on('click', function () {
+                   communicator.publish("click:pageTitle");
+                });
+             },
+             update : function (text) {
+                this.$pageTitle.text(text);
+             }
+          });
+       });
+                        
+                         
