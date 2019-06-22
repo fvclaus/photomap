e11bbf45d77d2def7af2aca2dfab2138cabe8da8
Frederik Claus <f.v.claus@googlemail.com>
2019-06-22 18:40:49 +0200

    Removes legacy project

--- photomap-legacy/public/javascripts/insert-photo.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("input[name=photo-album]").val($("p.mp-invisible").html());
-    $(".jquery-validator").validator();
-
-});
-$.tools.validator.fn("[type=file]",function(el,value){
-    return /\.(jpg|png)$/i.test(value) ? true : "only jpg or png allowed";
-});
