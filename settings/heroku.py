8cd925359b365ae950fd7473e404b32ff8dfa765
Frederik Claus <f.v.claus@googlemail.com>
2019-10-18 11:18:55 +0200

    Nonsense commit

--- settings/heroku.py
+++ settings/heroku.py
@@ -5,6 +5,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 COMPRESS_OFFLINE = True
 
+# BASE_DIR is required by django_heroku
 BASE_DIR = PROJECT_PATH
 
 django_heroku.settings(locals(), staticfiles=False)
